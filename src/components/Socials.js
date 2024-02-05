0cc1faae3409f3615c701d7b7515e207544ef0cd
Roman Medvedev <r.medvedev@nltu.lviv.ua>
2024-02-05 14:04:30 +0100

    start point

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useContext } from "react";
 
+import {
+  FaInstagram,
+  FaViber,
+  FaFacebook,
+  FaTwitter,
+  FaYoutube,
+  FaPinterest,
+} from "react-icons/fa";
+import { CursorContext } from "../context/CursorContext";
 const Socials = () => {
-  return <div>Socials</div>;
+  const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
+
+  return (
+    <div
+      onMouseEnter={mouseEnterHandler}
+      onMouseLeave={mouseLeaveHandler}
+      className="hidden xl:flex ml-24"
+    >
+      <ul className="flex gap-x-5">
+        <li>
+          <a
+            href="https://www.instagram.com/photographer_anna_vilkova/"
+            target="_blank"
+          >
+            {" "}
+            <FaInstagram />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.viber.com" target="_blank">
+            <FaViber />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/photographerannaivanova14"
+            target="_blank"
+          >
+            {" "}
+            <FaFacebook />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.pinterest.com" target="_blank">
+            {" "}
+            <FaPinterest />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com" target="_blank">
+            {" "}
+            <FaTwitter />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com" target="_blank">
+            {" "}
+            <FaYoutube />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Socials;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,85 @@
-import React from 'react';
+import React, { useContext } from "react";
 
+import { motion } from "framer-motion";
+
+import { transition1 } from "../transitions";
+
+import AnnaImage from "../img/contact/AnnaImage3.png";
+import { CursorContext } from "../context/CursorContext";
 const Contact = () => {
-  return <div>Contact</div>;
+  const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
+  return (
+    <motion.section
+      className="section bg-white"
+      initial={{ opacity: 0, y: "100%" }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: "100%" }}
+      transition={transition1}
+    >
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col lg:flex-row h-full items-center justify-center pt-36 gap-x-8 text-center lg:text-left">
+          <motion.div
+            initial={{ opacity: 0, y: "100%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "100%" }}
+            transition={transition1}
+            className="hidden lg:flex bg-[#eef7f9] absolute bottom-0 left-0 rifgt-0 top-72 z-10"
+          ></motion.div>
+          <div
+            onMouseEnter={mouseEnterHandler}
+            onMouseLeave={mouseLeaveHandler}
+            className="lg:flex-1 lg:pt-32 px-4"
+          >
+            <h1 className="h1">Contact me</h1>
+            <p className="mb-12">I would love to get suggestions from you.</p>
+            <form className="flex flex-col gap-y-4">
+              <div className="flex gap-x-10">
+                <input
+                  className="outline-none border-b
+                  border-b-primary h-[60px] bg-transparent
+                  font-secondary w-full pl-3
+                  placeholder:text-[#757879]"
+                  placeholder="Your name"
+                  type="text"
+                ></input>
+                <input
+                  className="outline-none border-b
+                  border-b-primary h-[60px] bg-transparent
+                  font-secondary w-full pl-3
+                  placeholder:text-[#757879]"
+                  placeholder="Your email address"
+                  type="text"
+                ></input>
+              </div>
+              <input
+                className="outline-none border-b
+                  border-b-primary h-[60px] bg-transparent
+                  font-secondary w-full pl-3
+                  placeholder:text-[#757879]"
+                placeholder="Your message"
+                type="text"
+              ></input>
+              <button className="btn mb-[30px] mx-auto lg:mx-0 self-start">
+                {" "}
+                Send it
+              </button>
+            </form>
+          </div>
+          <motion.div
+            onMouseEnter={mouseEnterHandler}
+            onMouseLeave={mouseLeaveHandler}
+            className="lg:flex-1"
+            initial={{ opacity: 0, y: "100%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "100%" }}
+            transition={{ transition: transition1, duration: 1.5 }}
+          >
+            <img src={AnnaImage} alt="" />
+          </motion.div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Contact;
